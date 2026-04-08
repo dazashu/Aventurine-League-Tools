d2c8effcdaaf7cedc33252e9e2e2c45de1ff5e12
budlibu500 <budlibu500@gmail.com>
2026-04-08 23:11:13 +0200

    version bump

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Aventurine: League Tools",
     "author": "Bud and Frog",
-    "version": (2, 7, 0),
+    "version": (2, 7, 1),
     "blender": (4, 0, 0),
     "location": "File > Import-Export",
     "description": "Plugin for working with League of Legends 3D assets natively",
